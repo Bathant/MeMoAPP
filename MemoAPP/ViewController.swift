15ab43b463c81b334bff225d1568babeb41daef1
Bathanti <bathanti@Bathantis-Mac.local>
2017-08-17 20:56:02 +0200

    Model Presentation to pass to another  controller

--- MemoAPP/ViewController.swift
+++ MemoAPP/ViewController.swift
@@ -8,33 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    var count : Int!
-    var label :UILabel!
-    override func viewDidLoad() {
+class ViewController: UIViewController,UIImagePickerControllerDelegate ,UINavigationControllerDelegate{
+        override func viewDidLoad() {
         super.viewDidLoad()
-        count = 0
-       //Label
-         label  = UILabel()
-        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
-        label.text = "0"
-        self.view.addSubview(label)
-        //Button
         
-        let Button : UIButton = UIButton()
-        Button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
-        Button.setTitle("Click", for: .normal)
-        Button.setTitleColor(UIColor.blue, for: .normal)
-        self.view.addSubview(Button)
-        
-        Button.addTarget(self, action: #selector(ViewController.changecounter), for: UIControlEvents.touchUpInside)
         
     }
-    func changecounter ()
-    {
-        self.count = count! + 1
-        self.label.text = "\(self.count!)"
+   
+    @IBAction func Exp(_ sender: Any) {
+        
+        //this will diplay Images in your Library
+        let nextcont = UIImagePickerController()
+        //this will display View Activity
+        let image = UIImage()
+        let cont =  UIActivityViewController(activityItems: [image] , applicationActivities : nil )
+        //this will show ALert
+        let alertcont = UIAlertController()
+        alertcont.title = "tHIS IS an ALert"
+        alertcont.message = "Hey u :D "
+        self.present(alertcont, animated: true, completion: nil)
     }
+    
 
 }
 
