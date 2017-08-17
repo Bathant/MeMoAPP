8a309057b72a908b76f37af813afe59f9845ad36
Bathanti <bathanti@Bathantis-Mac.local>
2017-08-17 19:25:54 +0200

    CounterL1

--- MemoAPP/ViewController.swift
+++ MemoAPP/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count : Int!
+    var label :UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        count = 0
+       //Label
+         label  = UILabel()
+        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        self.view.addSubview(label)
+        //Button
+        
+        let Button : UIButton = UIButton()
+        Button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
+        Button.setTitle("Click", for: .normal)
+        Button.setTitleColor(UIColor.blue, for: .normal)
+        self.view.addSubview(Button)
+        
+        Button.addTarget(self, action: #selector(ViewController.changecounter), for: UIControlEvents.touchUpInside)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func changecounter ()
+    {
+        self.count = count! + 1
+        self.label.text = "\(self.count!)"
     }
-
 
 }
 
