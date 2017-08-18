8780678a0f2b18d19935ce5b8aaae1cfa2de374b
Bathanti <bathanti@Bathantis-Mac.local>
2017-08-18 16:07:45 +0200

    Keyboard not working

--- MemoAPP/ViewController.swift
+++ MemoAPP/ViewController.swift
@@ -8,27 +8,90 @@
 
 import UIKit
 
-class ViewController: UIViewController,UIImagePickerControllerDelegate ,UINavigationControllerDelegate{
+class ViewController: UIViewController,UIImagePickerControllerDelegate ,UINavigationControllerDelegate,UITextFieldDelegate{
+    
+    
+    @IBOutlet weak var BotmTxtF: UITextField!
+    @IBOutlet weak var TopTxtF: UITextField!
+    @IBOutlet weak var ImagePickerView: UIImageView!
+    @IBOutlet weak var CamerBTN: UIBarButtonItem!
+    let memetextatt : [String : Any ]
+        = [NSStrokeColorAttributeName : UIColor.black ,
+           NSForegroundColorAttributeName : UIColor.white ,
+           NSFontAttributeName : UIFont(name : "HelveticaNeue-CondensedBlack" , size : 40) as Any ,
+            NSStrokeWidthAttributeName : -1.0]
         override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-    }
-   
-    @IBAction func Exp(_ sender: Any) {
-        
-        //this will diplay Images in your Library
-        let nextcont = UIImagePickerController()
-        //this will display View Activity
-        let image = UIImage()
-        let cont =  UIActivityViewController(activityItems: [image] , applicationActivities : nil )
-        //this will show ALert
-        let alertcont = UIAlertController()
-        alertcont.title = "tHIS IS an ALert"
-        alertcont.message = "Hey u :D "
-        self.present(alertcont, animated: true, completion: nil)
+        CamerBTN.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+            BotmTxtF.defaultTextAttributes = memetextatt
+            TopTxtF.defaultTextAttributes = memetextatt
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        subscribeToKeyNotif()
+    }
+   
+   
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        TopTxtF.text = ""
+        BotmTxtF.text = ""
+    }
+ /*
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        BotmTxtF.resignFirstResponder()
+        TopTxtF.resignFirstResponder()
+        return true
+    }*/
+    @IBAction func ToolPick(_ sender: Any) {
+        
+        let pickerImage = UIImagePickerController()
+        pickerImage.delegate = self
+        pickerImage.sourceType = .photoLibrary
+        self.present(pickerImage, animated: true, completion: nil)
+        
+    }
+    //move the view up when keyboard appears
+    func KeyboardwilShow(notification : Notification)
+    {
+        self.view.frame.origin.y = self.view.frame.origin.y - getKeyboardHeight(notification: notification)
+    }
+    
+    func subscribeToKeyNotif()
+    {
+        NotificationCenter.default.addObserver(self, selector: #selector(KeyboardwilShow), name: Notification.Name.UIKeyboardWillShow, object: nil)
+    }
+    func getKeyboardHeight(notification : Notification) -> CGFloat
+    {
+        let usrInfo = notification.userInfo
+        let keySize = usrInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        
+        return keySize.cgRectValue.height
+        
+        
+    }
+    
+    
+    @IBAction func CameraPicker(_ sender: Any) {
+        
+        let pickerImage = UIImagePickerController()
+        pickerImage.delegate = self
+        pickerImage.sourceType = .camera
+        self.present(pickerImage, animated: true, completion: nil)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard  let Img = info[UIImagePickerControllerOriginalImage] as? UIImage else { return}
+        ImagePickerView.image = Img
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true
+            , completion: nil)
+    }
 
 }
 
